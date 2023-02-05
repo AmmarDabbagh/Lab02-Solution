6ed0ce832d66dd84bbc3b8da92fae7e3b7a0e60a
OmarBaRaean <s202044700@kfupm.edu.sa>
2023-02-05 16:36:55 +0300

    2nd change

--- Sorter.java
+++ Sorter.java
@@ -1,7 +1,10 @@
 public class Sorter{
     public static void main(String[] args) {
         System.out.println("Hello");
+        insertionSort();
     }
+
+    public static void insertionSort() {System.out.println("insertionSort");    }
 
 
 }
